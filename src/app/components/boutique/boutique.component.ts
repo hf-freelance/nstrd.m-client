5afcec62550ce3a45dbdadd9b636f635859e6fb9
hf-freelance <hfazio.freelance@gmail.com>
2024-11-07 13:36:45 +0100

    Linking Boutique and Item

--- src/app/components/boutique/boutique.component.ts
+++ src/app/components/boutique/boutique.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ItemComponent } from './item/item.component';
 
 @Component({
   selector: 'app-boutique',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ItemComponent],
   templateUrl: './boutique.component.html',
   styleUrl: './boutique.component.css'
 })
--- src/app/components/boutique/item/item.component.ts
+++ src/app/components/boutique/item/item.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-boutique',
+  selector: 'app-item',
   standalone: true,
   imports: [],
   templateUrl: './item.component.html',
   styleUrl: './item.component.css'
 })
 export class ItemComponent {
+  @Input() name = '';
 }

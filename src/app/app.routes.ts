547b018922b37978a9eb0b925b6ac02e69a8531c
hf-freelance <hfazio.freelance@gmail.com>
2024-11-07 13:29:29 +0100

    Navbar and navigation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LinogravureComponent } from './components/linogravure/linogravure.component';
+import { CyanotypeComponent } from './components/cyanotype/cyanotype.component';
+import { CeramiqueComponent } from './components/ceramique/ceramique.component';
+import { EditionComponent } from './components/edition/edition.component';
+import { HomeComponent } from './components/home/home.component';
+import { BoutiqueComponent } from './components/boutique/boutique.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'linogravure',
+        component: LinogravureComponent,
+    },
+    {
+        path: 'cyanotype',
+        component: CyanotypeComponent,
+    },
+    {
+        path: 'ceramique',
+        component: CeramiqueComponent,
+    },
+    {
+        path: 'edition',
+        component: EditionComponent,
+    },
+    {
+        path: 'boutique',
+        component: BoutiqueComponent,
+    },
+];
